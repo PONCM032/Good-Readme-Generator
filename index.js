c841509dc918674a56222c1f68d55bdcc98f610f
Melanie <melanie-ponce@hotmail.com>
2020-05-29 09:10:24 -0400

    update formatting

--- index.js
+++ index.js
@@ -2,80 +2,150 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
-
+//
 const writeFileAsync = util.promisify(fs.writeFile);
-const {questions} = require("./Questions");
+// const { questions } = require("./Questions");
 
 //call questions array in Questions.js
-function promptUser(){
-    return inquirer.prompt(questions);
-};
+// function promptUser() {
+//   return inquirer.prompt(questions);
+// };
 
-function generateReadMe(answers) {
-    return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-      <p class="lead">I am from ${answers.location}.</p>
-      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${answers.github}</li>
-        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-      </ul>
-    </div>
-  </div>
-  </body>
-  </html>`;
+function promptUser() {
+  return inquirer.prompt([
+  {
+      type: "input",
+      name: "name",
+      message: "What is your GitHub username?",
+      validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+  },
+  {
+      type: "input",
+      name: "email",
+      message: "What is your email?"
+  },
+  {
+      type: "input",
+      name: "URL",
+      message: "What is the URL to your project?"
+  },
+  {
+      type: "input",
+      name: "project",
+      message: "What is your project's name?"
+  },
+  {
+      type: "input",
+      name: "description",
+      message: "Please write a short description of your project"
+  },
+  {
+      type: "list",
+      name: "license",
+      message: "What kind of license should your project have?",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+      type: "input",
+      name: "command",
+      message: "What command should be run to install dependencies?",
+      default: "npm i"
+  },
+  {
+      type: "input",
+      name: "test",
+      message: "What command should be run to run tests?",
+      default: "npm test"
+  },
+  {
+      type: "input",
+      name: "using",
+      message: "What does the user need to know about using the repo?"
+  },
+  {
+      type: "input",
+      name: "contributing",
+      message: "What does the user need to know about contributing to the repo?"
   }
+]);
+}
+
+
+
+  function generateReadMe(answer) {
+    return `
+  **#${answer.project}**
+
+  ${answer.description}
+
+  ##${answer.name}
+
+  ---
+  
+  This site was built using [GitHub Pages](${answer.URL}).
+
+  
+  ${answer.license}
+  ${answer.command}
+  ${answer.test}
+  ${answer.using}
+  ${answer.contributing}
+    `
+  };
+
+promptUser().then(function(answers) {
+  const readMe = generateReadMe(answers);
+
+  return writeFileAsync("README.md", readMe);
+}).then(function(){
+  console.log("ReadMe has been successfully generated!");
+}).catch(function(err) {
+  console.log(err);
+});
 
 //# creates heading
 //## creates desciptions
 
 //psuedocode
 //create npm package.json
-    //will have all dependencies
-    //install inquirer
+//will have all dependencies
+//install inquirer
 
-    //have .gitignore for node_modules
+//have .gitignore for node_modules
 
 //require your dependencies
 //write read me file - fs
 
 
 //write down prompts to get required info from user
-    //array
-        //each question is an object
-            //type, name, message (autofillers)
+//array
+//each question is an object
+//type, name, message (autofillers)
 
 //Build out ReadMe template
-    //make it look like how you want it
-    //then bring it into JS file
+//make it look like how you want it
+//then bring it into JS file
 
 //write the file
-    //plug and play with gathered info
+//plug and play with gathered info
 
+// fs.writeFile("README.md", generateReadMe, function(err) {
+//  if(err){
+//    console.log(err);
+//   }else{
+//     console.log("ReadMe has been successfully generated!");
+//   }
+// }); //init();
 
-    async function init (){
-        try{
-            const answer = await promptUser();
-            const readMe = generateReadMe(answers);
-            await writeFileAsync("build/README.md", readMe);
-            console.log("Wrote ReadMe!");
-        }catch(err){
-            console.log(err);
-        } 
-    } 
-    init();
-    
+// async function init() {
+//   t
+// init();
+
 // function writeToFile(fileName, data) {
 // }
 
@@ -84,3 +154,13 @@ function generateReadMe(answers) {
 // }
 
 // init();
+
+
+// try {
+//   // const answer = await promptUser();
+//   // const readMe = generateReadMe(answers);
+//   // await writeFileAsync("build/README.md", readMe);
+//   console.log("ReadMe has been successfully generated!");
+// } catch (err) {
+//   console.log(err);
+// }
